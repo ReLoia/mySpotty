ede5b7b6ba0ee896f1772f253844592c705ce9f8
ReLoia <zrennyz@gmail.com>
2026-02-17 18:31:52 +0100

    Bump version to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "it.reloia.myspotty"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.2.0"
+        versionCode = 3
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
